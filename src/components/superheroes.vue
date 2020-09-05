36e57ba08553c6f945e74a466fbc212d70ffb7da
samanta <samanta.villegasm@gmail.com>
2020-09-05 16:49:09 -0300

    initial

--- src/components/superheroes.vue
+++ src/components/superheroes.vue
@@ -2,7 +2,8 @@
   <v-container fluid>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-card  v-for="superhero in superheroes" :key="superhero.id" class="center">
+        <v-card  dark class="mx-auto my-12" width="400" v-for="superhero in superheroes" :key="superhero.id">
+          <v-img align-self="start" class="fill-width" height="400px" transition="scale-transition" aspect-ratio="2" :src="superhero.avatarURL"/>
           <h1> {{ superhero.nombre }} </h1>
           <v-card-title>
             {{superhero.nombre}}
@@ -15,7 +16,6 @@
           </v-card-text>
           <v-list-content  v-if="superhero.puedeVolar === true" >
             <div class="headline mb-1"> Habilidades </div>
-            <div class="overline mb-4"> {{ hab }} </div>
             <div class="overline mb-4"> {{superhero.habilidades[0]}} </div>
             <div class="overline mb-4"> {{superhero.habilidades[1]}} </div>
             <div class="overline mb-4"> {{superhero.habilidades[2]}} </div>
@@ -64,3 +64,10 @@
     }   
   }
 </script>
+<style scoped lang="scss">
+  .my-img {
+    display: flex;
+    vertical-align: top;
+  }
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/'
 }
